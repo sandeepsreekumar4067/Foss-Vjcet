203139d1633759635695fc907ec1eb3d48e31c34
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-11-30 21:04:36 +0530

    made the skeleton for front page

--- src/Pages/HomePage.vue
+++ src/Pages/HomePage.vue
@@ -1,13 +1,29 @@
 <template>
   <div class="home-page">
-    Hello world
+    <div class="header-container">
+      <div class="slogan">
+        New Way To Learn Online
+      </div>
+      <div class="front-image">
+        <img :src="frontImage" alt="">
+      </div>
+    </div>
+    <div class="dept-container">
+      dept
+    </div>
   </div>
 </template>
 
 <script>
 import '../Style/HomePage.css'
+import '../Style/app.css'
+import frontImage from '../assets/front image.png'
 export default {
-
+  data(){
+    return{
+      frontImage:frontImage
+    }
+  }
 }
 </script>
 
