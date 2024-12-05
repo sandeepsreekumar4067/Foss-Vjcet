eff109a5e259b2df189618acaddca468cb67eefe
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-12-05 20:05:05 +0530

    made the material display page

--- src/Pages/SemesterPage.vue
+++ src/Pages/SemesterPage.vue
@@ -1,12 +1,49 @@
 <template>
-    {{ semId }}
+    <div class="semester-page">
+        <div class="sem-title">
+            {{ semId }}
+        </div>
+        <div class="sem-notes">
+            <span>Select the Study material you want</span>
+            <div v-for="note in materials" :key="note.name" class="material-tile">
+                {{ note.name }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
+import '../Style/semesterpage.css'
 export default {
     data(){
         return{
-            semId:this.$route.params.id
+            semId:this.$route.params.id,
+            materials : [
+                {
+                    name:"material 1",
+                    link:'#'
+                },
+                {
+                    name:"material 1",
+                    link:'#'
+                },
+                {
+                    name:"material 1",
+                    link:'#'
+                },
+                {
+                    name:"material 1",
+                    link:'#'
+                },
+                {
+                    name:"material 1",
+                    link:'#'
+                },
+                {
+                    name:"material 1",
+                    link:'#'
+                }
+            ]
         }
     }
 }
