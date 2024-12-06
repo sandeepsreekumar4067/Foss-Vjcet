1afcc8d3b4f4a5a2ff93238ab76b33b72c0936a5
sandeepsreekumar4067 <sandeepsreekumar4067@gmail.com>
2024-12-06 22:21:20 +0530

    made the download page structure

--- src/Pages/DownloadPage.vue
+++ src/Pages/DownloadPage.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="download-page">
-    
+    {{ name }}
   </div>
 </template>
 
 <script>
 export default {
+    mounted(){
+        const modules = this.$route.query.modules?
+        JSON.parse(this.$route.query.modules):[]
+        console.log(modules);
+        this.modules=modules
+    },
     data(){
         return{
-            name: this.$route.params.name
+            name: this.$route.params.name,
+            modules:[]
         }
     }
 }
--- src/Pages/MaterialPage.vue
+++ src/Pages/MaterialPage.vue
@@ -4,44 +4,40 @@
       {{ materialName }}
     </div>
     <div class="material-notes">
-     <div v-for="note in material.notes" :key="note.name" class="note-tile">
-      {{ note.name }}
-     </div>
+      <div @click="()=>{gotoDownloadPage(note)}" v-for="note in material.notes" :key="note.name" class="note-tile">
+        {{ note.name }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import '../Style/materialpage.css'
+import "../Style/materialpage.css";
 export default {
-  mounted(){
-    const material = this.$route.query.material?
-    JSON.parse(this.$route.query.material):[]
+  mounted() {
+    const material = this.$route.query.material
+      ? JSON.parse(this.$route.query.material)
+      : [];
     console.log(material);
-    this.material=material
+    this.material = material;
   },
-  methods:{
-    gotoDownloadPage(name){
+  methods: {
+    gotoDownloadPage(note) {
       this.$router.push({
-        name:'Download',
-        params:{name:name},
-        query:{modules:''}
-      })
-    }
+        name: "Download",
+        params: { name: note.name },
+        query: { modules: JSON.stringify(note.modules)},
+      });
+    },
   },
-  data(){
-    return{
-      materialName:this.$route.params.name,
-      material:[],
-      kerlaNotes:[],
-      ktuNotes:[],
-      fossNotes:[],
-      teachersNotes:[]
-    }
-  }
-}
+  data() {
+    return {
+      materialName: this.$route.params.name,
+      material: [],
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
